f0a0a0b107bd93a5569d8191c12bc2d6ae5ef66c
ytyaru <yttry0@gmail.com>
2023-04-27 15:30:52 +0900

    lang logline-table

--- docs/1/js/language.js
+++ docs/1/js/language.js
@@ -1,13 +1,28 @@
 (function() {
 class Language {
     constructor() {this.langs = {'en-us':{'flag':'🇺🇸','trans':null}, 'ja-jp':{'flag':'🇯🇵','trans':null}}; this.select = null;}
-    setup() {
-        //document.querySelector('header').appendChild(this.createSelect())
+    async setup() {
         document.querySelector('#header').appendChild(this.#createSelect())
         this.#addEvent()
+        this.load(this.Default)
+    }
+    get Default() {
+        let langs = Object.keys(this.langs).filter(l=>l.includes(window.navigator.language))
+        return (0===langs.length) ? 'en-us' : langs[0]
+    }
+    async load(lang) {
+        console.log(lang)
+        console.log(this.langs[lang].trans)
+        if (!this.langs[lang].trans) {
+            const res = await fetch(`locales/${lang}/translation.json`)
+            console.log(this.langs)
+            console.log(lang)
+            this.langs[lang].trans = await res.json()
+        }
     }
     #createSelect() {
         this.select = document.createElement('select')
+        this.select.id = 'language'
         for (let key of Object.keys(this.langs)) {
             const option = document.createElement('option')
             option.textContent = this.langs[key].flag + ' ' + key
@@ -18,12 +33,14 @@ class Language {
         console.log(window.navigator.language)
         let myLang = Object.keys(this.langs).filter(l=>l.includes(window.navigator.language))
         if (!myLang) { myLang = 'en-us' }
-        this.select.value = myLang
+        this.select.value = this.Default
         this.select.dispatchEvent(new Event('change'))
         return this.select
     }
     #addEvent() {
         this.select.addEventListener('change', async(e) => {
+            console.log(e.target.value)
+            console.log(this.langs[e.target.value].trans)
             if (!this.langs[e.target.value].trans) {
                 const res = await fetch(`locales/${e.target.value}/translation.json`)
                 console.log(this.langs)
--- docs/1/js/logline-table.js
+++ docs/1/js/logline-table.js
@@ -2,22 +2,19 @@
 class LoglineTable {
     constructor() { this.tables = new Map() }
     async setup() {
-        const table = document.getElementById('logline-table')
-        table.parentElement.insertBefore(this.#makeStyle(), table), 
-        this.#setInputWidth()
+        document.getElementById('logline-table-css-container').appendChild(this.#makeStyle())
         this.#setupEvent()
     }
     #makeStyle() {
         const style = document.createElement('style')
         const css = `:root { --logline-table-input-width:50vw; --logline-table-display:inherit; }
 #logline-table { width:100%; border-collapse:collapse; display:var(--logline-table-display);}
-#logline-table :is(table,tr,td,input) { padding:0; margin:0; }
-/*#logline-table tr td input[type=text] { width:var(--logline-table-input-width); }*/
-#logline-table tr td:nth-child(1) { width:var(--logline-table-input-width); }
-#logline-table.ja-jp tr td:nth-child(1) { width:var(--logline-table-input-width); background:#FF0000; }
-#logline-table.en-us tr td:nth-child(2) { width:var(--logline-table-input-width); background:#FFFF00; }
+#logline-table :is(table,tr,td,span,input) { padding:0; margin:0; }
+/*#logline-table tr td:nth-child(1) { width:var(--logline-table-input-width); }*/
+#logline-table.ja-jp tr td:nth-child(1) { width:var(--logline-table-input-width); }
+#logline-table.en-us tr td:nth-child(2) { width:var(--logline-table-input-width); }
 #logline-table tr td { white-space:nowrap; }
-#logline-table tr td input[type=text] { width:100%; display:table-cell; }
+#logline-table tr td input[type=text] { width:var(--logline-table-input-width); }
 `
         style.textContent = css
         return style
@@ -28,26 +25,13 @@ class LoglineTable {
         const spans = [...document.querySelectorAll('#logline-table tr td span')]
         const spanWidth = Math.max(...spans.map(span=>span.getBoundingClientRect().right - span.getBoundingClientRect().left))
         const width = `calc(50vw - ${firstLeft}px - ${spanWidth}px)`
-        console.log(width)
         document.querySelector(':root').style.setProperty('--logline-table-input-width', width)
     }
-    /*
-    #setInputWidth() {
-        const firstTd = document.querySelector('#logline-table tr td:first-child')
-        const firstLeft = firstTd.getBoundingClientRect().left
-        console.log([...document.querySelectorAll('#logline-table tr td span')])
-        console.log([...[...document.querySelectorAll('#logline-table tr td span')].map(span=>span.getBoundingClientRect().right - span.getBoundingClientRect().left)])
-        console.log(Math.max(...[...[...document.querySelectorAll('#logline-table tr td span')].map(span=>span.getBoundingClientRect().right - span.getBoundingClientRect().left)]))
-        const spanWidth = Math.max(...[...[...document.querySelectorAll('#logline-table tr td span')].map(span=>span.getBoundingClientRect().right - span.getBoundingClientRect().left)])
-        const width = `calc(50vw - ${firstLeft}px - ${spanWidth}px)`
-        document.querySelector(':root').style.setProperty('--logline-table-input-width', width)
-    }
-    */
     #setupEvent() {
         const ui = document.getElementById('is-show-logline-table')
         ui.addEventListener('change', async(event) => {
             document.querySelector(':root').style.setProperty('--logline-table-display', (event.target.checked) ? 'inherit' : 'none')
-            //this.#getInputWidth(event.target.checked) 
+            if (document.getElementById('language')) { this.reset(document.getElementById('language').value) }
         })
         ui.checked = false
         ui.dispatchEvent(new Event('change'))
@@ -64,19 +48,15 @@ class LoglineTable {
         table.classList.add(lang)
         const ids = ['stasis-death', 'flawed-protagonist', 'break-into-2', 'modpoint-happen', 'all-is-lost', 'theme']
         const colors = ['pink', 'pink', 'green', 'green', 'green', 'blue', 'green']
-        if ('en-us'===lang) { ids.splice(4, 'he-she'); colors.splice(4, 'green');  }
+        if ('en-us'===lang) { ids.splice(4, 0, 'he-she'); colors.splice(4, 0, 'green');  }
         for (let i=0; i<ids.length; i++) {
-            //const method = ('ja-jp'===lang) ? this.#createTrJa : this.#createTrEn
-            //table.appendChild(method.call(this, lang, `logline-${ids[i]}`, colors[i]))
             table.appendChild(this.#createTr(lang, `logline-${ids[i]}`, colors[i]))
         }
         return table
     }
-
     #createTr(lang, id, color) {
         const label = Language.langs[lang].trans.form.label[id]
         const placeholder = Language.langs[lang].trans.form.placeholder[id]
-        console.log(lang, id, color, label, placeholder)
         const tds = [this.#createTdInput, this.#createTdSpan]
         const tdParams = [[id, placeholder], [label]]
         if ('en-us'===lang) { tds.reverse(); tdParams.reverse(); }
@@ -85,24 +65,6 @@ class LoglineTable {
         for (let i=0; i<tds.length; i++) {
             tr.appendChild(tds[i].call(this, ...tdParams[i]))
         }
-        return tr
-    }
-    #createTrJa(lang, id, color) {
-        const tr = document.createElement('tr')
-        const label = Language.langs[lang].trans.form.label[id]
-        const placeholder = Language.langs[lang].trans.form.placeholder[id]
-        tr.classList.add(color)
-        tr.appendChild(this.#createTdInput(id, placeholder))
-        tr.appendChild(this.#createTdSpan(label))
-        return tr
-    }
-    #createTrEn(item) {
-        const tr = document.createElement('tr')
-        const label = Language.langs[lang].trans.form.label[id]
-        const placeholder = Language.langs[lang].trans.form.placeholder[id]
-        tr.classList.add(color)
-        tr.appendChild(this.#createTdInput(id, placeholder))
-        tr.appendChild(this.#createTdSpan(label))
         return tr
     }
     #createTdSpan(text) {
@@ -114,23 +76,14 @@ class LoglineTable {
     }
     #createTdInput(id, item) {
         const td = document.createElement('td')
-        /*
-        if ('he-she'===id) { td.appendChild(this.#createTdHeSheSelect(id)) }
-        else {
-            const input = document.createElement('input')
-            input.id = id
-            input.name = id
-            input.placeholder = text
-            td.appendChild(input)
-        }
-        */
-        td.appendChild(('he-she'===id) ? this.#createTdHeSheSelect(id) : this.#createInput(id, item))
+        td.appendChild(('logline-he-she'===id) ? this.#createTdHeSheSelect(id) : this.#createInput(id, item))
         return td
     }
     #createInput(id, placeholder) {
         const input = document.createElement('input')
         input.id = id
         input.name = id
+        input.type = 'text'
         input.placeholder = placeholder
         return input
     }
